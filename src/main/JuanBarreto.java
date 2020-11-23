776cede99940c017dd838f0646d23178ad21e69c
Julia Paradelo <julia.paradelo@alunos.ifsuldeminas.edu.br>
2020-11-23 18:22:16 -0300

    Eu, julia estou enviando a minha classe e o meu "sout", e o da lavinia tambem.

--- src/main/JuanBarreto.java
+++ src/main/JuanBarreto.java
@@ -13,5 +13,8 @@ public class JuanBarreto {
     public static void main(String[] args) {
         
         System.out.println("Olá, Juan aqui!");
+        System.out.println("Olá, Julia aqui!");
+        System.out.println("Olá, Lavínia aqui!");
     }
+
 }
